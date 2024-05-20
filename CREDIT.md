b16e37fcb811afa18ec862ebe505fb03f217681c
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2024-05-20 14:36:46 -0700

    add markdowns for Amaterasu's gui (#8)

---
+++ CREDIT.md
@@ -0,0 +1,21 @@
+## Author
+* Name: DocilElm
+* Contributions: Everything
+* Links: §nhttps://github.com/DocilElm
+
+## Project Repos:
+* §nhttps://github.com/DocilElm/Amaterasu
+* §nhttps://github.com/DocilElm/DocGuiLib
+
+## Contributors
+* Name: nwjn
+  * Contributions: something something
+  * Links: §nhttps://github.com/nwjn
+
+* Name: Squagward
+  * Contributions: cool guy
+  * Links: §nhttps://github.com/camnwalter
+
+## Inspirations
+* Mod: Vigilance
+  * Links: §nhttps://github.com/EssentialGG/Vigilance
--- amaterasu.js
+++ amaterasu.js
@@ -1,11 +1,16 @@
 import Settings from "./core/Settings"
 
 const version = JSON.parse(FileLib.read("Amaterasu", "metadata.json")).version
-const changelog = `# §6Amaterasu-v${ version }\n` + FileLib.read("Amaterasu", "changelog.md")
-const readme = FileLib.read("Amaterasu", "README.md")
+
+// markdowns
+const CHANGELOG = `# §6Amaterasu-v${ version }\n` + FileLib.read("Amaterasu", "changelog.md")
+const README = FileLib.read("Amaterasu", "README.md")
+const LICENSE = FileLib.read("Amaterasu", "LICENSE")
+const CREDITS = FileLib.read("Amaterasu", "CREDIT.md")
+
 const defaultConfig = JSON.parse(FileLib.read("Amaterasu", "data/defaultSettings.json"))
 
-const schemes = ["data/ColorScheme.json", "data/scheme-Pink.json", "data/scheme-vigil.json"]
+const schemes = ["data/ColorScheme.json", "data/scheme-vigil.json", "data/scheme-nwjn.json"]
 
 const config = new Settings("Amaterasu", "data/settings.json", "data/ColorScheme.json", defaultConfig)
     .setCommand("amaterasu", ["amat"])
@@ -16,8 +21,10 @@ const config = new Settings("Amaterasu", "data/settings.json", "data/ColorScheme
     .onClick("GUI", "apply", apply)
     .onClick("General", "redirect", () => config.redirect("GUI", "height"))
 
-    .addMarkdown("Changelog", changelog)
-    .addMarkdown("README", readme)
+    .addMarkdown("Changelog", CHANGELOG)
+    .addMarkdown("LICENSE", LICENSE)
+    .addMarkdown("README", README)
+    .addMarkdown("Credits", CREDITS)
 
 function apply() {
     const conf = config.settings
